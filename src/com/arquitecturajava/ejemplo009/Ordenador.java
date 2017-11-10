a4b8195a6016d8679e8e3366743e60f8e3350690
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-10 09:43:26 -0500

    ejemplo con peek para ver como el flujo va haciendo su trabajo

---
+++ src/com/arquitecturajava/ejemplo009/Ordenador.java
@@ -0,0 +1,59 @@
+package com.arquitecturajava.ejemplo009;
+
+public class Ordenador {
+	
+	private String marca;
+	private double precio;
+	private int disco;
+	private int memoria;
+	
+	public Ordenador() {
+		super();
+	}
+
+	public Ordenador(String marca, double precio, int disco, int memoria) {
+		super();
+		this.marca = marca;
+		this.precio = precio;
+		this.disco = disco;
+		this.memoria = memoria;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getDisco() {
+		return disco;
+	}
+
+	public void setDisco(int disco) {
+		this.disco = disco;
+	}
+
+	public int getMemoria() {
+		return memoria;
+	}
+
+	public void setMemoria(int memoria) {
+		this.memoria = memoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Ordenador [marca=" + marca + ", precio=" + precio + ", disco=" + disco + ", memoria=" + memoria + "]";
+	}
+
+}
---
+++ src/com/arquitecturajava/ejemplo009/Principal.java
@@ -0,0 +1,32 @@
+package com.arquitecturajava.ejemplo009;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Ordenador o1 = new Ordenador("Lenovo", 700, 256, 8);
+		Ordenador o2 = new Ordenador("Apple", 1300, 256, 8);
+		Ordenador o3 = new Ordenador("Dell", 1000, 512, 16);
+		Ordenador o4 = new Ordenador("HP", 800, 512, 8);
+		
+		List<Ordenador> listaOrdenador = new ArrayList<>();
+		listaOrdenador.add(o1);
+		listaOrdenador.add(o2);
+		listaOrdenador.add(o3);
+		listaOrdenador.add(o4);
+		
+		listaOrdenador.stream()
+					  .peek(System.out::println)
+					  .filter((o) -> o.getPrecio() >= 1000)
+					  .peek(System.out::println)
+				      .map((o) -> o.getPrecio() * 1.21)
+				      .reduce(Double::sum)
+				      .ifPresent(System.out::println);
+					 
+
+	}
+
+}
