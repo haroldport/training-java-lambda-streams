75c9c85d46ad7419a61b299d9cac7bc0d21d2aa4
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-10 09:24:43 -0500

    ejemplo de streams operaciones filter, map y forEach

---
+++ src/com/arquitecturajava/ejemplo008/Principal.java
@@ -0,0 +1,29 @@
+package com.arquitecturajava.ejemplo008;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Ordenador o1 = new Ordenador("Lenovo", 700, 256, 8);
+		Ordenador o2 = new Ordenador("Apple", 1300, 256, 8);
+		Ordenador o3 = new Ordenador("Dell", 1000, 512, 16);
+		Ordenador o4 = new Ordenador("HP", 800, 512, 8);
+		
+		List<Ordenador> listaOrdenador = new ArrayList<>();
+		listaOrdenador.add(o1);
+		listaOrdenador.add(o2);
+		listaOrdenador.add(o3);
+		listaOrdenador.add(o4);
+		
+		listaOrdenador.stream()
+					  .filter((o) -> o.getPrecio() >= 1000)
+				      .map((o) -> new Ordenador(o.getMarca(), o.getPrecio() * 1.21, o.getDisco(), o.getMemoria()))
+				      .forEach(System.out::println);
+					 
+
+	}
+
+}
