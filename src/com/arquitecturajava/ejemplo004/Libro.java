83dbbf993fe92a64d20389d79506575c66262c6f
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-10 08:19:33 -0500

    ejemplo de genericos y expresiones lambda

---
+++ src/com/arquitecturajava/ejemplo004/Libro.java
@@ -0,0 +1,50 @@
+package com.arquitecturajava.ejemplo004;
+
+public class Libro {
+	
+	private String categoria;
+	private int paginas;
+	private String titulo;
+	private double precio;
+	
+	public Libro(String categoria, int paginas, String titulo, double precio) {
+		super();
+		this.categoria = categoria;
+		this.paginas = paginas;
+		this.titulo = titulo;
+		this.precio = precio;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public int getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+}
